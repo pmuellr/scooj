cbd0c9f8b4c4d118e362d0414c876a902b64e981
Patrick Mueller <pmuellr@yahoo.com>
2010-07-02 18:46:46 -0400

    initial load

---
+++ README.md
@@ -0,0 +1,75 @@
+scooj - Simple Classical OO for JavaScript
+===============================================================================
+
+Despite the ongoing hate for the "pseudo-classical" object oriented
+class definition style in JavaScript from all the kewl kids, one of my
+thoughts is that JavaScript actual needs some order brought to it, for
+today's JavaScript mega-programs.  Because there is no order, and
+programs become as maintainable as BASICA programs of yore. It's the
+wild west.  Classical OO brings some order.  This is an experiment.
+
+**`scooj`** is currently targeted to browser JavaScript, as compared to
+CommonJS-y, module-y JavaScript.  When I finally implement or find a nice
+small require() system for browser JavaScript, I'll write a version of
+**`scooj`** which is module-friendly.
+
+**`scooj`** is a JavaScript library that let's you define "classes" that have
+instance methods and static methods.  Classes can have superclasses, and
+a real super method invocation is available.
+
+See the [examples](test/scooj.sample/sample.html) for some basic use cases.
+
+Usage
+-------------------------------------------------------------------------------
+
+Add a link to **`scooj.js`** to your HTML file.
+
+**`scooj`** defines all of it's functions in the "**`scooj`**" global 
+variable.  You can use the function **`scooj.installGlobals()`** to have
+some of the functions defined globally as well.
+
+Functions defined
+-------------------------------------------------------------------------------
+
+**`scooj.defPackage(packageName)`**
+
+Defines a new package.  All classes added after this function is
+executed will be added to this package.  The package defines the
+global name the classes will be available in.
+
+**`scooj.defClass([superclass,] constructorFunction)`**
+
+Defines a new function.  If subclassing an existing class, specify that
+class as the first parameter.  The constructorFunction will be the
+constructor for the class.
+
+All methods added after this function is executed will be added to this
+class.
+
+**`scooj.defStaticMethod(methodFunction)`**
+
+Define a static method on the current class.
+
+**`scooj.defMethod(methodFunction)`**
+
+Define an instance method on the current class.
+
+**`scooj.defSuper()`**
+
+Returns a function which performs super invocation.  The super
+function should be invoked with the following parameters:
+the receiver (should be `this`), the method name to
+be invoked, and then any parameters to be passed to the method.
+
+**`scooj.installGlobals()`**
+
+Makes all of the **`scooj.def*()`** functions available globally, instead
+of just as properties of the **`scooj`** object.
+
+Copyright / License
+-------------------------------------------------------------------------------
+
+Copyright (c) 2010 Patrick Mueller
+
+Licensed under the 
+[MIT license](http://www.opensource.org/licenses/mit-license.php)
---
+++ scooj.js
@@ -0,0 +1,262 @@
+//-----------------------------------------------------------------------------
+// Copyright (c) 2010 Patrick Mueller
+// Licensed under the MIT license: 
+// http://www.opensource.org/licenses/mit-license.php
+//-----------------------------------------------------------------------------
+
+(function(){
+    
+if (this.scooj) return
+
+this.scooj = {}
+
+//----------------------------------------------------------------------------
+// scooj properties
+//----------------------------------------------------------------------------
+scooj._global         = this
+scooj._packages       = {}
+scooj._classes        = {}
+scooj._currentPackage = null
+scooj._currentClass   = null
+
+scooj.debug           = false
+
+//----------------------------------------------------------------------------
+// shortcut for defining scooj functions
+//----------------------------------------------------------------------------
+function defScooj(func) {
+    ensureNamedFunction(func)
+    
+    scooj[func.name] = func
+}
+
+//----------------------------------------------------------------------------
+// scooj.defPackage(string)
+//----------------------------------------------------------------------------
+defScooj(function defPackage(name) {
+    scooj._currentPackage = name
+})
+
+//----------------------------------------------------------------------------
+// scooj.defClass(function)
+//----------------------------------------------------------------------------
+defScooj(function defClass(superclass, func) {
+    if (null == func) {
+        func = superclass
+        superclass = null
+    }
+    
+    if (superclass) {
+        if (!superclass._scooj) throw new Error("superclass is not a defined class")
+        if (!superclass._scooj.isClass) throw new Error("superclass is not a defined class")
+    }
+
+    ensureNamedFunction(func)
+    
+    var fullClassName = getFullClassName(null, func.name)
+    
+    if (scooj._classes.hasOwnProperty(fullClassName)) {
+        throw new Error("class is already defined: " + fullClassName)
+    }
+    
+    func._scooj = {}
+    func._scooj.isClass = true
+    func._scooj.owningClass = func
+    func._scooj.superclass = superclass
+    func._scooj.subclasses = {}
+    func._scooj.name = func.name
+    func._scooj.packageName = scooj._currentPackage
+    func._scooj.fullClassName = fullClassName
+    func._scooj.methods = {}
+    func._scooj.staticMethods = {}
+    
+    scooj._classes[fullClassName] = func
+    
+    scooj._currentClass = func
+    
+    if (superclass) {
+        var T = function() {}
+    
+        T.prototype = superclass.prototype
+        func.prototype = new T()
+        func.prototype.constructor = func
+        
+        scooj.defStaticMethod(getSuperMethod(func))
+    }
+
+    defineGlobalFunction(func)
+    
+    return func
+})
+
+//----------------------------------------------------------------------------
+// scooj.defMethod(function)
+//----------------------------------------------------------------------------
+defScooj(function defMethod(func) {
+    addMethod(func, false)
+})
+
+//----------------------------------------------------------------------------
+// scooj.defStaticMethod(function)
+//----------------------------------------------------------------------------
+defScooj(function defStaticMethod(func) {
+    addMethod(func, true)
+})
+
+//----------------------------------------------------------------------------
+// return a super invoker
+//----------------------------------------------------------------------------
+defScooj(function defSuper(args) {
+    var klass = ensureClassCurrentlyDefined()
+
+    return getSuperMethod(klass)
+})
+        
+
+//----------------------------------------------------------------------------
+// scooj.installGlobals()
+//----------------------------------------------------------------------------
+defScooj(function installGlobals() {
+    var globalNames = [
+        "defPackage",
+        "defClass",
+        "defSuperclass",
+        "defMethod",
+        "defStaticMethod",
+        "defSuper"
+    ]
+    
+    for (var i=0; i<globalNames.length; i++) {
+        var name = globalNames[i]
+        var func = scooj[name]
+        
+        scooj._global[name] = func
+    }
+})
+
+
+//============================================================================
+// utilities
+//============================================================================
+
+//----------------------------------------------------------------------------
+// define a function globally
+//----------------------------------------------------------------------------
+function defineGlobalFunction(func) {
+    var packageName = func._scooj.packageName
+    
+    if (null == packageName) {
+        scooj._global[func.name] = func
+        return
+    }
+    
+    var lastContainer = scooj._global
+    var pieces = packageName.split(".")
+    for (var i=0; i<pieces.length; i++) {
+        var piece = pieces[i]
+        if (!lastContainer[piece]) lastContainer[piece] = {}
+        lastContainer = lastContainer[piece]
+    }
+    
+    lastContainer[func.name] = func
+}
+
+//----------------------------------------------------------------------------
+// return a new $super method
+//----------------------------------------------------------------------------
+function getSuperMethod(owningClass) {
+    var superclass = owningClass._scooj.superclass
+    
+    return function $super(thisp, methodName) {
+        var superFunc = superclass.prototype[methodName]
+        if (!superFunc) superFunc = superclass
+
+        return superFunc.apply(thisp, Array.prototype.splice.call(arguments, 2))
+    }
+}
+
+//----------------------------------------------------------------------------
+// add a method to a class
+//----------------------------------------------------------------------------
+function addMethod(func, isStatic) {
+    isStatic = !!isStatic
+    
+    ensureNamedFunction(func)
+    var klass = ensureClassCurrentlyDefined()
+    
+    var methodContainer = isStatic ? klass._scooj.staticMethods : klass._scooj.methods
+
+    if (methodContainer.hasOwnProperty(func.name)) {
+        throw new Error("method is already defined in class: " + klass.name + "." + func.name)
+    }
+    
+    func._scooj = {}
+    func._scooj.owningClass = klass
+    func._scooj.isMethod = true
+    func._scooj.isStatic = isStatic
+    
+    methodContainer[func.name] = func
+    
+    if (isStatic) {
+        klass[func.name] = func
+    }
+    
+    else {
+        klass.prototype[func.name] = func
+    }
+}
+
+//----------------------------------------------------------------------------
+// ensure no methods
+//----------------------------------------------------------------------------
+function ensureNoMethods() {
+    var klass = ensureClassCurrentlyDefined()
+    
+    for (var name in klass.methods) {
+        if (!klass.methods.hasOwnProperty(name)) continue
+        
+        throw new Error("class already has methods defined: " + klass.name)
+    }
+}
+
+
+//----------------------------------------------------------------------------
+// ensure parameter is a named function
+//----------------------------------------------------------------------------
+function ensureNamedFunction(func) {
+    if (typeof func != "function") throw new Error("expecting a function: " + func)
+    if (!func.name) throw new Error("function must not be anonymous: " + func)
+    
+    return func.name
+}
+
+//----------------------------------------------------------------------------
+// ensure a class is currently defined
+//----------------------------------------------------------------------------
+function ensureClassCurrentlyDefined() {
+    if (!scooj._currentClass) throw new Error("no class currently defined")
+    
+    return scooj._currentClass
+}
+
+//----------------------------------------------------------------------------
+// return full class name
+//----------------------------------------------------------------------------
+function getFullClassName(packageName, className) {
+    if (null == packageName) packageName = scooj._currentPackage
+    
+    if (null == packageName) return className
+    
+    return packageName + "." + className
+}
+
+
+//----------------------------------------------------------------------------
+// debug logger
+//----------------------------------------------------------------------------
+function debugLog(message) {
+    if (!scooj.debug) return
+    console.log(message)
+}
+
+})()
